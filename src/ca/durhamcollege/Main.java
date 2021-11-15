1bac10393098723c60b3b5a18e29a53e46d54d3a
mokrooussovi <ivan.mokrooussov@dcmail.ca>
2021-11-15 10:31:44 -0500

    updates on Vector2D class

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -4,6 +4,7 @@
  * @IvanStudentID: 100808605
  * @date: November 15, 2021
  * @description: Classes and Objects in Java
+ * @based on OOP3200-F2020-ICE9 by Tom Tsiliopoulos
  */
 
 package ca.durhamcollege;
